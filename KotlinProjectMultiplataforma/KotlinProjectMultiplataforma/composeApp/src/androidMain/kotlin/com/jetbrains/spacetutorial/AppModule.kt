485930984314552d43a93ad9280d350a700937bc
Roman <Roman@DESKTOP-RS415KS>
2024-05-16 16:24:11 -0600

    La App de multiplataforma, saul has el documento

---
+++ KotlinProjectMultiplataforma/KotlinProjectMultiplataforma/composeApp/src/androidMain/kotlin/com/jetbrains/spacetutorial/AppModule.kt
@@ -0,0 +1,20 @@
+package com.jetbrains.spacetutorial
+
+import com.jetbrains.spacetutorial.cache.AndroidDatabaseDriverFactory
+import com.jetbrains.spacetutorial.network.SpaceXApi
+import org.koin.android.ext.koin.androidContext
+import org.koin.dsl.module
+import org.koin.androidx.viewmodel.dsl.viewModel
+
+val appModule = module {
+    single<SpaceXApi> { SpaceXApi() }
+    single<SpaceXSDK> {
+        SpaceXSDK(
+            databaseDriverFactory = AndroidDatabaseDriverFactory(
+                androidContext()
+            ), api = get()
+        )
+    }
+    viewModel { RocketLaunchViewModel(sdk = get()) }
+}
+
---
+++ KotlinProjectMultiplataforma/KotlinProjectMultiplataforma/shared/build.gradle.kts
@@ -0,0 +1,71 @@
+
+plugins {
+    alias(libs.plugins.kotlinMultiplatform)
+    alias(libs.plugins.androidLibrary)
+    alias(libs.plugins.kotlinxSerialization)
+    alias(libs.plugins.sqldelight)
+}
+
+kotlin {
+    androidTarget {
+        compilations.all {
+            kotlinOptions {
+                jvmTarget = "11"
+            }
+        }
+    }
+    
+    listOf(
+        iosX64(),
+        iosArm64(),
+        iosSimulatorArm64()
+    ).forEach { iosTarget ->
+        iosTarget.binaries.framework {
+            baseName = "Shared"
+            isStatic = true
+        }
+    }
+    
+    sourceSets {
+        commonMain.dependencies {
+            implementation(libs.kotlinx.coroutines.core)
+            implementation(libs.ktor.client.core)
+            implementation(libs.ktor.client.content.negotiation)
+            implementation(libs.ktor.serialization.kotlinx.json)
+            implementation(libs.runtime)
+            implementation(libs.kotlinx.datetime)
+            implementation(libs.koin.core)
+        }
+        androidMain.dependencies {
+            implementation(libs.ktor.client.android)
+            implementation(libs.android.driver)
+            implementation(libs.androidx.compose.material3)
+            implementation(libs.koin.androidx.compose)
+            implementation(libs.androidx.lifecycle.viewmodel.compose)
+        }
+        iosMain.dependencies {
+            implementation(libs.ktor.client.darwin)
+            implementation(libs.native.driver)
+        }
+    }
+}
+
+android {
+    namespace = "org.example.project.shared"
+    compileSdk = libs.versions.android.compileSdk.get().toInt()
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_11
+        targetCompatibility = JavaVersion.VERSION_11
+    }
+    defaultConfig {
+        minSdk = libs.versions.android.minSdk.get().toInt()
+    }
+}
+
+sqldelight {
+    databases {
+        create("AppDatabase") {
+            packageName.set("com.jetbrains.spacetutorial.cache")
+        }
+    }
+}
